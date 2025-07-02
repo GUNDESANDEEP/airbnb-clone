d35138974b613f660f1aa4eea4397049317342b5
Gunde Sandeep <gundesandeep2005@gmail.com>
2025-07-02 16:53:04 +0530

    Part 6: Private Route protection completed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,61 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import AddProperty from "./pages/AddProperty";
 import Dashboard from "./pages/Dashboard";
 import MyListings from "./pages/MyListings";
-import EditProperty from "./pages/EditProperty"; // 👈 NEW
+import EditProperty from "./pages/EditProperty";
+
+import Navbar from "./components/Navbar";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
+
       <Routes>
+        {/* Public Routes */}
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/add-property" element={<AddProperty />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/my-listings" element={<MyListings />} />
-        <Route path="/edit/:index" element={<EditProperty />} /> {/* 👈 NEW ROUTE */}
-      <Route path="/edit/:id" element={<EditProperty />} />
+
+        {/* Protected Routes */}
+        <Route
+          path="/add-property"
+          element={
+            <PrivateRoute>
+              <AddProperty />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/my-listings"
+          element={
+            <PrivateRoute>
+              <MyListings />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/edit/:id"
+          element={
+            <PrivateRoute>
+              <EditProperty />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
